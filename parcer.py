8f1d6929721dabc25eef0f6116520740c0e98803
Arthur Oliveira De Rosso <arthur.oliveira-de-rosso@grenoble-inp.org>
2023-03-24 15:09:40 +0100

    Finish parcer

--- parcer.py
+++ parcer.py
@@ -1,27 +1,54 @@
 import yaml
 
-def dfs(graph, node, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(node)
-    print(node)
-    for neighbor in graph[node]['accessTo']:
-        neighbor_node = neighbor[0]
-        if neighbor_node not in visited:
-            dfs(graph, neighbor_node, visited)
+def all_paths(dag):
+    # Find the suitable source and destination vertices
+    sources = [v for v in dag if not any(v in edges for edges in dag.values())]
+    destinations = [v for v in dag if not dag[v]]
+    if len(sources) != 1 or len(destinations) != 1:
+        raise ValueError("DAG must have exactly one source and one destination vertex.")
+    s, d = sources[0], destinations[0]
+
+    # Find all paths from the source to the destination
+    def find_paths_helper(curr, path, paths):
+        if curr == d:
+            paths.append(path + [curr])
+            return
+        for neighbor in dag[curr]:
+            find_paths_helper(neighbor, path + [curr], paths)
+
+    paths = []
+    find_paths_helper(s, [], paths)
+    return paths
+
+dag = {
+    'f0': ['f1', 'C'],
+    'f1': ['A', 'B'],
+    'A': ['j0'],
+    'B': ['j0'],
+    'j0': ['j1'],
+    'C': ['j1'],
+    'j1': []
+}
 
 
-with open('mia_result.yaml') as file:
-    data = yaml.safe_load(file) # Load the YAML file safely
+# with open('input_example.yaml') as file:
+#     data = yaml.safe_load(file) # Load the YAML file safely
 
-nodes = data['nodes'] # Retrieve the 'nodes' key from the dictionary
+# # Retrieve the 'nodes' key from the dictionary
+# nodes = [node for node in data['nodes'].values()]
 
-# Find the root node
-# you can find it by iterating over all the nodes in the JSON file and looking for the one that has no dependencies (i.e., the root node)
-root_node = None
-for node_name, node_data in nodes.items():
-    if not node_data['deps']:
-        root_node = node_name
-        break
+# # Initialize the DAG dictionary
+# dag = {node['traceName']: [] for node in nodes}
 
-dfs(nodes, root_node)
+# # Extract the edges from the YAML data
+# for node in nodes:
+#     for dest_node, _ in node['accessTo']:
+#         dag[node['traceName']] += [dest_node]
+
+# # Print the resulting DAG
+# print(dag)
+
+paths = all_paths(dag)
+print("All paths in the DAG:")
+for path in paths:
+    print(path)
